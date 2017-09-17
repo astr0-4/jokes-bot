9abc5a2bc1c891ade96033fa129325d087c0d8a2
Alex Stroulger <alexstroulger@gmail.com>
2017-09-17 10:52:53 -0400

    fetching jokes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var SlackBot = require("slackbots")
 var request = require("request")
-var endpoint = "https://icanhazdadjoke.com/"
+var endpoint = "https://icanhazdadjoke.com/slack"
 
 const envKey = process.env.JOKES_BOT_TOKEN
 
@@ -9,25 +9,30 @@ var bot = new SlackBot({
   token: envKey,
   name: "Jokes Bot"
 })
-postMessage = message => {
-  return bot.postMessageToUser("alex", message, params)
+
+bot.on("message", msg => {
+  switch (msg.type) {
+    case "message":
+      // we only want to listen to direct messages that come from the user
+      if (msg.channel[0] === "D" && msg.bot_id === undefined) {
+        getRandomJoke(postMessage, msg.user)
+      }
+      break
+  }
+})
+
+const postMessage = (message, user) => {
+  bot.postMessage(user, message, { as_user: true })
 }
 
-getRandomJoke = callback => {
-  return request("https://icanhazdadjoke.com/", (error, response) => {
+const getRandomJoke = (callback, user) => {
+  return request(endpoint, (error, response) => {
     if (error) {
       console.log("Error: ", error)
     } else {
-      const joke = JSON.parse(response.body)
-      console.log(joke)
-      return callback(joke)
+      let jokeJSON = JSON.parse(response.body)
+      let joke = jokeJSON.attachments[0].text
+      return callback(joke, user)
     }
   })
 }
-bot.on("start", function() {
-  // more information about additional params https://api.slack.com/methods/chat.postMessage
-  var params = {
-    icon_emoji: ":joy:"
-  }
-  getRandomJoke(postMessage)
-})
